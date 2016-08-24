4141b909e5a8cc7d21cc0252a823304300d78672
Rangel <rangel.varnier@gmail.com>
2016-08-24 20:53:14 -0300

    criacao model negociacao com objeto imutavel

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,31 @@
+class Negociacao {
+    /**
+     * Classe com valores imutaveis.
+     * atribuindo a data um new Date(...gerTime()) para garantir via
+     * programação defensiva que a data não será alterada via referencia
+     * fora da classe
+     */
+    constructor(data, quantidade, valor) {
+        this._data = new Data(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+}
