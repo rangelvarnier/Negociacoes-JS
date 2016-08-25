80b7868c8c38c1a23df6d5575fbaba5578c22c77
Rangel <rangel.varnier@gmail.com>
2016-08-24 21:39:59 -0300

    cache de objetos do DOM no construtor

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,17 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this.inputData = $("#data");
+        this.inputQuantidade = $("#quantidade");
+        this.inputValor = $("#valor");
+    }
+
+    adicionar(event) {
+        event.preventDefault();
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
+    }
+}
