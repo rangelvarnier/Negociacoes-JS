4df5ad6a9699650be2f50f0503b9b4cc87bb3130
Rangel <rangel.varnier@gmail.com>
2016-08-28 23:18:47 -0300

    aplicando proxy de interceptação no Controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,22 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
 
-        /*
-        * implementar proxy
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model));
-        */
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+
+                if ('[adicionar, esvasiar]'.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function() {
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
 
