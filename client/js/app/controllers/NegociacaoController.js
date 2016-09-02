2430dc768be7ac20b14c98beda836d6f8681c4d2
Rangel <rangel.varnier@gmail.com>
2016-09-01 23:08:22 -0300

    importação de negociação usando ajax e adicionando na lista de negociaçoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -21,6 +21,25 @@ class NegociacaoController {
         this._limparFormulario();
     }
 
+    importarNegociacoes() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    JSON.parse(xhr.responseText).map(objeto =>
+                        new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)
+                    ).forEach(negociacao => this._listaNegociacoes.adicionar(negociacao));
+                    this._mensagem.texto = "Negociações importadas com sucesso!";
+                } else {
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possivel obter as negociaçoes da semana'
+                }
+            }
+        }
+        xhr.send();
+    }
+
     apagar() {
         this._listaNegociacoes.esvasiar();
         this._mensagem.texto = "Negociações apagadas com sucesso!";
