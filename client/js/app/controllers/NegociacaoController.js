5c91e3a8161a77406e3231a4b24d066c0654d6d0
Rangel <rangel.varnier@gmail.com>
2016-10-09 20:43:05 -0300

    refatoração de legibilidade usando retorno da promisse

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,15 +15,11 @@ class NegociacaoController {
 
         ConnectionFactory
             .getConnection()
-            .then(connection => {
-                new NegociacaoDao(connection)
-                    .listaTodos()
-                    .then(negociacoes => {
-                        negociacoes.forEach(negociacao => {
-                            this._listaNegociacoes.adicionar(negociacao);
-                        });
-                    });
-            });
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adicionar(negociacao)));
     }
 
     adicionar(event) {
